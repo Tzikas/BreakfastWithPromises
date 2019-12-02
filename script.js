00f78e4d0c692c95fce0d00f59fd209e4c2c81d5
kikibres <kristinabressler@yahoo.com>
2019-12-02 16:02:34 -0500

    completed

--- script.js
+++ script.js
@@ -33,13 +33,86 @@ const mashPotatoes = [
   'enjoy'
 ] 
 
-  
-function makeFood(steps, id){
-  console.log('start here', steps, id)
+//to understand Callbacks
+function makeFoodSteak(steps, id) {
+  addFood(steps[0], id, function(){
+    addFood(steps[1], id, function(){
+      addFood(steps[3], id, function() {
+        addFood(steps[4], id, function() {
+          addFood(steps[5], id, function() {
+            addFood(steps[6], id, function() {
+              addFood(steps[7], id, function() {
+                document.querySelector('#table').innerHTML += '<img src="images/steak.jpg" />'
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+}
+
+//to understand Promises and then
+function makeFoodPotatoes(steps, id) {
+  addFood(steps[0], id).then(()=>{
+    addFood(steps[1], id).then(()=>{
+      addFood(steps[2], id).then(()=>{
+        addFood(steps[3], id).then(()=>{
+          addFood(steps[4], id).then(()=>{
+            document.querySelector('#table').innerHTML += '<img src="images/mashPotatoes.jpg" />'
+          })
+        })
+      })
+    })
+  })
+}
+
+//to understand Async and Await
+async function makeFoodBrussels(steps, id) {
+  await addFood(steps[0], id)
+  await addFood(steps[1], id)
+  await addFood(steps[2], id)
+  await addFood(steps[3], id)
+  await addFood(steps[4], id)
+  await addFood(steps[5], id)
+  await addFood(steps[6], id)
+  await addFood(steps[7], id)
+  await addFood(steps[8], id)
+  document.querySelector('#table').innerHTML += '<img src="images/brusselSprouts.jpg" />'
 }
 
 
+// makeFoodSteak(steak, "#steak");
+// makeFoodPotatoes(mashPotatoes, "#mashPotatoes");
+// makeFoodBrussels(brusselSprouts, "#brusselSprouts");
 
-makeFood(steak, '#steak')
+async function makeFood(steps, id){
+  // console.log('start here', steps, id);
+  let promises = [];
+  for (step of steps){
+    promises.push( await addFood(step, id) )
+  }
+  document.querySelector('#table').innerHTML += `<img src="images/${id.replace('#','')}.jpg" />`;
+  return promises;
+
+  // addFood()
+}
+
+//to understand Promises.all
+Promise.all([
+  makeFood(steak, "#steak"),
+  makeFood(mashPotatoes, "#mashPotatoes"),
+  makeFood(brusselSprouts, "#brusselSprouts")
+]).then(val=>{
+  console.log('all done', val)
+    document.querySelector('body').innerHTML += `<button >Dinner is Served!</button>`
+    document.querySelector('button').onclick = function(){
+      new Audio('dinnerIsServed.mp3').play()
+    }
+})
+
+
+
+// makeFood(steak, '#steak')
 
 
